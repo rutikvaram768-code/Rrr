9f916b911440743fb8779e0439eaf3edd474960c
rutikvaram768-code <rutikvaram768@gmail.com>
2026-02-08 10:46:41 +0530

    Update rules.js

--- models/commands/rules.js
+++ models/commands/rules.js
@@ -19,13 +19,13 @@ module.exports.run = async({ api, event, args, client, Users, Threads, __GLOBAL,
   const request = global.nodemodule["request"];
   const fs = global.nodemodule["fs-extra"];
   var Uzairrajput3 = (`𝗕𝗢𝗧 𝗥𝗨𝗟𝗘𝗦\nplease read till the end\n\n『 • 』  𝗱𝗼𝗻'𝘁 resend the 𝗯𝗼𝘁 𝗺𝗲𝘀𝘀𝗮𝗴𝗲\n『 • 』  𝗱𝗼𝗻'𝘁 abuse like spamming a 𝗶𝗺𝗮𝗴𝗲 𝗿𝗲𝗾𝘂𝗲𝘀𝘁 you can request multiple but 𝗱𝗼𝗻'𝘁 spam it\n\nthe 𝗿𝘂𝗹𝗲𝘀 is very simple just 𝗱𝗼𝗻'𝘁 try to copy and resend the 𝗯𝗼𝘁 𝗺𝗲𝘀𝘀𝗮𝗴𝗲 if you get 𝗯𝗮𝗻 by my 𝘀𝗲𝗿𝘃𝗲𝗿 i will 𝗻𝗼𝘁 going to 𝘂𝗻𝗯𝗮𝗻 𝘆𝗼𝘂\n\nif you have a problem you can 𝗰𝗼𝗻𝘁𝗮𝗰𝘁 𝘁𝗵𝗲 𝗱𝗲𝘃𝗲𝗹𝗼𝗽𝗲𝗿\nhttps://www.facebook.com/profile.php?id=61552682190483`);
- var Uzairrajput = [
+ var rutik varma = [
 "https://i.imgur.com/huumLca.jpg",
 "https://i.imgur.com/EcryTGh.jpg",
 "https://i.imgur.com/tu12HrQ.jpg",
 "https://i.imgur.com/Vx25FHG.jpg",
 "https://i.imgur.com/NcbC8Pn.jpg",
   ];
-  var Uzairrajput2 = () => api.sendMessage({ body: Uzairrajput3, attachment: fs.createReadStream(__dirname + "/cache/Uzairrajput1.jpg")}, event.threadID, () => fs.unlinkSync(__dirname + "/cache/Uzairrajput1.jpg"), event.messageID);
+  var Rutik2 = () => api.sendMessage({ body: Uzairrajput3, attachment: fs.createReadStream(__dirname + "/cache/Uzairrajput1.jpg")}, event.threadID, () => fs.unlinkSync(__dirname + "/cache/Uzairrajput1.jpg"), event.messageID);
   return request(encodeURI(rutik varma[Math.floor(Math.random() * Uzairrajput.length)])).pipe(fs.createWriteStream(__dirname + "/cache/rutik varma1.jpg")).on("close", () => rutik varma2());
 };
